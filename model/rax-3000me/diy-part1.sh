1e3126e92fc603505e3eee02cc058f9c444b5a7b
Steven <admin@linbei.vip>
2026-08-25 08:41:32 +0000

    test

--- model/rax-3000me/diy-part1.sh
+++ model/rax-3000me/diy-part1.sh
@@ -7,9 +7,7 @@ dir_pwd=`pwd`
 echo "--------------current directory is $dir_pwd --------------"
 
 #Add
-mydir='package/dev'
-mkdir -p "$mydir"&&cd "$mydir"
-mkdir my-config
+cd "package/dev"
 git clone --depth=1 --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git
 git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth=1 --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall2.git
